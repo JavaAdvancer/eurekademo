c851e9050afb3552004b199630e6717d6235b807
liubinghao <advancer_lbh@hotmail.com>
2019-01-23 17:24:15 +0800

    Spring Cloud (五)：路由网关（Zuul）

---
+++ eureka-zuul/src/main/java/com/lbh/eurekazuul/filter/MyFilter.java
@@ -0,0 +1,72 @@
+package com.lbh.eurekazuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by Advancer on 2019/1/21 10:24.
+ * auth: lbh
+ */
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accountToken = request.getParameter("token");
+        if (null == accountToken) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        log.info("ok");
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
